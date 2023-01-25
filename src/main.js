f15ce2f197e08d94d3e3b4bc2f481cafca2c0592
侯东洋 <thebug0000@gmail.com>
2023-01-25 15:12:34 +0800

    fix:去除element plus

--- src/main.js
+++ src/main.js
@@ -1,26 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'virtual:windi.css'
 import router from "./router";
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import {
-  // create naive ui
-  create,
-  // component
-  NButton
-} from 'naive-ui'
+import { setupNaive } from '@/utils/NaiveUIComponents';
 
-const naive = create({
-  components: [NButton]
-})
-const app = createApp(App)
+  const app = createApp(App)
 
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
-app.use(router);
-app.use(ElementPlus);
-app.use(naive);
-app.mount('#app')
+  // 注册全局常用的 naive-ui 组件
+  setupNaive(app);
+  app.use(router);
+  app.mount('#app');
