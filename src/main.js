59f4ea3825a012102394c7a65838ef10c8196b78
侯东洋 <thebug0000@gmail.com>
2023-01-25 15:13:19 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'element-plus/dist/index.css'
 import 'virtual:windi.css'
 import router from "./router";
 import { setupNaive } from '@/utils/NaiveUIComponents';
